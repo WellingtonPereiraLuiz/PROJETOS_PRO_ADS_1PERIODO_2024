3c6c6134477e3d5e0a1bc388ea50daccc2050a1d
wellington pereira luiz <108313369+WellingtonPereiraLuiz@users.noreply.github.com>
2024-09-08 22:38:45 -0400

    basico

---
+++ trabalho final/script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Previne o envio padrão do formulário
+    alert('Formulário enviado com sucesso!'); // Exibe uma mensagem de alerta
+});
